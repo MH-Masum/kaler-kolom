34e6fdfcf2457673ff3db238b603bc30a1f3fec4
MH-Masum <mhmahim13119@gmail.com>
2024-02-03 00:17:26 +0600

    news-project-kaler-kolom

---
+++ script.js
@@ -0,0 +1,36 @@
+$(".owl-carousel").owlCarousel({
+  
+  autoplay: true,
+  loop: true,
+  margin: 20,
+  
+    responsive: {
+        0: {
+          items: 2,
+        },
+        550: {
+          items: 3,
+        },
+        600: {
+          items: 3,
+        },
+        700: {
+          items:3,
+        },
+        768: {
+          items:4,
+        },
+        906: {
+          items: 4,
+        },
+        1000: {
+          items: 6,
+        },
+        1100: {
+          items: 6,
+        },
+        1306: {
+          items: 6,
+        },
+      },
+});
